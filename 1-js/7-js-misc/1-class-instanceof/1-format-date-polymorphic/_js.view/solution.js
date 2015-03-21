9122b131d0fe25d87f7c20fb73edb7c1be6cd802
Ilya Kantor <iliakan@gmail.com>
2015-03-22 00:36:11 +0300

    renovations

---
+++ 1-js/7-js-misc/1-class-instanceof/1-format-date-polymorphic/_js.view/solution.js
@@ -0,0 +1,28 @@
+function formatDate(date) {
+  if (typeof date == 'number') {
+    // перевести секунды в миллисекунды и преобразовать к Date
+    date = new Date(date * 1000);
+  } else if (typeof date == 'string') {
+    // разобрать строку и преобразовать к Date
+    date = date.split('-');
+    date = new Date(date[0], date[1] - 1, date[2]);
+  } else if (Array.isArray(date)) { 
+    date = new Date(date[0], date[1], date[2]);
+  }
+  // преобразования для поддержки полиморфизма завершены, 
+  // теперь мы работаем с датой (форматируем её)
+
+  var day = date.getDate();
+  if (day < 10) day = '0' + day;
+
+  var month = date.getMonth() + 1;
+  if (month < 10) month = '0' + month;
+
+  // взять 2 последние цифры года
+  var year = date.getFullYear() % 100;
+  if (year < 10) year = '0' + year;
+
+  var formattedDate = day + '.' + month + '.' + year;
+
+  return formattedDate;
+}
