cfe125bc7d33694377100075197cf736c8aa27f5
Ilya Kantor <iliakan@gmail.com>
2015-04-28 19:14:55 +0300

    typo

--- 2-ui/3-event-details/3-mousemove-mouseover-mouseout-mouseenter-mouseleave/mouseleave.view/script.js
+++ 2-ui/3-event-details/3-mousemove-mouseover-mouseout-mouseenter-mouseleave/mouseleave.view/script.js
@@ -1,4 +1,4 @@
 function log(event) {
-  text.value += event.type + ' [target: ' + event.target.className + ']\n';
+  text.value += event.type + ' [target: ' + event.target.id + ']\n';
   text.scrollTop = text.scrollHeight;
 }
