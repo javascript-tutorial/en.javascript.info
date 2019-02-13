bddc40c467c8105098807f2a6186d700fb64fa61
CyberMew <CyberMew@users.noreply.github.com>
2019-02-13 15:40:31 +0800

    Update solution.js
    Change `let` to `const`.

--- 1-js/05-data-types/09-destructuring-assignment/6-max-salary/_js.view/solution.js
+++ 1-js/05-data-types/09-destructuring-assignment/6-max-salary/_js.view/solution.js
@@ -3,7 +3,7 @@ function topSalary(salaries) {
   let max = 0;
   let maxName = null;
 
-  for(let [name, salary] of Object.entries(salaries)) {
+  for(const [name, salary] of Object.entries(salaries)) {
     if (max < salary) {
       max = salary;
       maxName = name;
