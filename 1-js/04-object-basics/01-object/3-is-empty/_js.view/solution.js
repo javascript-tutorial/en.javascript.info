360be9ed996fcb2d93da31e91264ede725486b7e
Ilya Kantor <iliakan@gmail.com>
2019-02-21 18:47:25 +0300

    fix

--- 1-js/04-object-basics/01-object/3-is-empty/_js.view/solution.js
+++ 1-js/04-object-basics/01-object/3-is-empty/_js.view/solution.js
@@ -1,6 +1,6 @@
 function isEmpty(obj) {
   for (let key in obj) {
-    // if the loop has started, there is a prorty
+    // if the loop has started, there is a property
     return false;
   }
   return true;
