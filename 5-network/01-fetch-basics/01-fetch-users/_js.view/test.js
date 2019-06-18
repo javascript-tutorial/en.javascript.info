f3bfb60ca2301b69a49da84105d3219a225ddf60
Ilya Kantor <iliakan@gmail.com>
2019-06-18 10:13:43 +0300

    minor

--- 5-network/01-fetch-basics/01-fetch-users/_js.view/test.js
+++ 5-network/01-fetch-basics/01-fetch-users/_js.view/test.js
@@ -1,7 +1,7 @@
 describe("getUsers", function() {
 
   it("gets users from GitHub", async function() {
-    let users = getUsers(['iliakan', 'remy', 'no.such.users']);
+    let users = await getUsers(['iliakan', 'remy', 'no.such.users']);
     assert.equal(users[0].login, 'iliakan');
     assert.equal(users[1].login, 'remy');
     assert.equal(users[2], null);
