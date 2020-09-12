9319b40a95c930c06921bdbcbaf9eeb28199c770
Nguyễn Đức Hào <39975444+haond10adp@users.noreply.github.com>
2020-09-12 09:49:11 +0700

    Update script.js
    minor improvements

--- 2-ui/3-event-details/3-mousemove-mouseover-mouseout-mouseenter-mouseleave/mouseenter-mouseleave-delegation-2.view/script.js
+++ 2-ui/3-event-details/3-mousemove-mouseover-mouseout-mouseenter-mouseleave/mouseenter-mouseleave-delegation-2.view/script.js
@@ -29,15 +29,8 @@ table.onmouseout = function(event) {
   if (!currentElem) return;
 
   // we're leaving the element – where to? Maybe to a descendant?
-  let relatedTarget = event.relatedTarget;
-
-  while (relatedTarget) {
-    // go up the parent chain and check – if we're still inside currentElem
-    // then that's an internal transition – ignore it
-    if (relatedTarget == currentElem) return;
-
-    relatedTarget = relatedTarget.parentNode;
-  }
+  let relatedTarget = event.relatedTarget.closest('td');
+  if (relatedTarget == currentElem) return;
 
   // we left the <td>. really.
   onLeave(currentElem);
