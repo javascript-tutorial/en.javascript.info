acf62b0d03b8ad01ba6ec0cad7f1a92f10c3b4d0
Ilya Kantor <iliakan@gmail.com>
2021-01-21 22:49:27 +0300

    minor fixes

--- 1-js/05-data-types/10-destructuring-assignment/6-max-salary/_js.view/solution.js
+++ 1-js/05-data-types/10-destructuring-assignment/6-max-salary/_js.view/solution.js
@@ -1,16 +1,14 @@
 function topSalary(salaries) {
 
-  let max = 0;
+  let maxSalary = 0;
   let maxName = null;
 
   for(const [name, salary] of Object.entries(salaries)) {
     if (max < salary) {
-      max = salary;
+      maxSalary = salary;
       maxName = name;
     }
   }
 
   return maxName;
 }
-
-
