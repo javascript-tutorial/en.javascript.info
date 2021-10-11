29b8e2d48f915e6623d2e7c8afd1b4d1152111e2
Mohd Usman <84888797+aulth@users.noreply.github.com>
2021-10-11 20:36:12 +0530

    Update solution.js
    Added another method to get an unique array.

--- 1-js/05-data-types/07-map-set/01-array-unique-map/_js.view/solution.js
+++ 1-js/05-data-types/07-map-set/01-array-unique-map/_js.view/solution.js
@@ -1,3 +1,11 @@
 function unique(arr) {
   return Array.from(new Set(arr));
 }
+
+
+/* Another Solution */
+function unique(arr){
+let a = [];
+new Set(arr).forEach(value=>a.push(value));
+return a;
+}
