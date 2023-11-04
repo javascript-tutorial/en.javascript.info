8ab6b392334b0660d48ecc7b7278806645dce5e1
Oleksandr Tkachenko <wolfriend.official@gmail.com>
2023-11-04 13:40:51 +0200

    Add WeakRef and FinalizationRegistry article

---
+++ 1-js/99-js-misc/07-weakref-finalizationregistry/weakref-dom.view/index.js
@@ -0,0 +1,24 @@
+const startMessagesBtn = document.querySelector('.start-messages'); // (1)
+const closeWindowBtn = document.querySelector('.window__button'); // (2)
+const windowElementRef = new WeakRef(document.querySelector(".window__body")); // (3)
+
+startMessagesBtn.addEventListener('click', () => { // (4)
+    startMessages(windowElementRef);
+    startMessagesBtn.disabled = true;
+});
+
+closeWindowBtn.addEventListener('click', () =>  document.querySelector(".window__body").remove()); // (5)
+
+
+const startMessages = (element) => {
+    const timerId = setInterval(() => { // (6)
+        if (element.deref()) { // (7)
+            const payload = document.createElement("p");
+            payload.textContent = `Message: System status OK: ${new Date().toLocaleTimeString()}`;
+            element.deref().append(payload);
+        } else { // (8)
+            alert("The element has been deleted."); // (9)
+            clearInterval(timerId);
+        }
+    }, 1000);
+};
